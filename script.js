c5c105e1621c5e875cb8d8d708dceb6b6931cf38
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-05-25 15:25:12 +0100

    made chages in script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 import { pokemonArray } from "./data/pokemon.js";
 // console.log(pokemonArray);
+
+// need to do a for loop on object
+// need innerHTML to display it on the webpage
+
+const pokemonImage = document.querySelector(".card__image");
+const pokemonName = document.querySelector(".card__heading");
+const pokemonCard = document.querySelector(".card");
+const pokemonTypes = document.querySelector(".card__text");
+
+for (key in pokemonArray) {
+  console.log(key);
+}
