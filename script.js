faeec4c40c0dca14aca55d85ca23a860550f64b2
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-05-25 16:03:21 +0100

    made changes in script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-import { pokemonArray } from "./data/pokemon.js";
-pokemonArray = require("./data/pokemon.js")
+import pokemonArray from "./data/pokemon.js";
 // console.log(pokemonArray);
 
 // need to do a for loop on object
 // need innerHTML to display it on the webpage
 
-const pokemonImage = document.querySelector(".card__image");
-const pokemonName = document.querySelector(".card__heading");
-const pokemonCard = document.querySelector(".card");
-const pokemonTypes = document.querySelector(".card__text");
+// const pokemonImage = document.querySelector(".card__image");
+// const pokemonName = document.querySelector(".card__heading");
+// const pokemonCard = document.querySelector(".card");
+// const pokemonTypes = document.querySelector(".card__text");
 
-const keys = Object.keys(pokemonArray);
-console.log(keys);
+const entries = Object.entries(pokemonArray);
+console.log(entries);
