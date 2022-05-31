e11024a9eeed56340a8414e8516c7fe77b719244
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-05-31 22:12:24 +0100

    post deadline - added the array in the main.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const displayCard = document.querySelector(".card__content");
 
 // create a variable that will go through the object in the array
 
-const pokemonPrintCard = () => {
-  let printTheCard = pokemonArray
+const pokemonPrintCard = (array) => {
+  const printTheCard = array
     .map((pokemon) => {
       return ` 
     <main class="card-container">
@@ -15,7 +15,7 @@ const pokemonPrintCard = () => {
       <img class="card__image" src="${pokemon.sprite}" alt="Image of pokemon"/>
       <div class="card__content">
         <div class="card__heading">${pokemon.name}</div>
-        <p class="card__text">${pokemon.name} (#${pokemon.id}) is a ${pokemon.type[0]} & ${pokemon.type[1]} type pokemon</p>
+        <p class="card__text">${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} & ${pokemon.types[1]} type pokemon </p>
       </div>
     `;
     })
@@ -24,5 +24,5 @@ const pokemonPrintCard = () => {
   return printTheCard;
 };
 
-const characterCards = pokemonPrintCard(pokemonArray);
-displayCard.innerHTML = characterCards;
+const pokemonCards = pokemonPrintCard(pokemonArray);
+displayCard.innerHTML = pokemonCards;
