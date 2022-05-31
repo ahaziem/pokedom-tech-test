d90a1bcdac133495b8934eca57203cf8ff9190e7
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-05-31 21:51:49 +0100

    post deadline - made change in main.js

--- script.js
+++ script.js
@@ -5,32 +5,17 @@ import pokemonArray from "./data/pokemon.js";
 const displayCard = document.querySelector(".card__content");
 
 // create a variable that will go through the object in the array
-const nameOfPokemon = pokemonArray.map((nameOfPokemon) => {
-  return nameOfPokemon.name;
-});
 
-const idOfPokemon = pokemonArray.map((idOfPokemon) => {
-  return idOfPokemon.id;
-});
-
-const typeOfPokemon = pokemonArray.map((typeOfPokemon) => {
-  return typeOfPokemon.types;
-});
-
-const imageOfPokemon = pokemonArray.map((imageOfPokemon) => {
-  return imageOfPokemon.sprite;
-});
-
-const pokemonPrintCard = (array) => {
-  let printTheCard = array
-    .map(() => {
+const pokemonPrintCard = () => {
+  let printTheCard = pokemonArray
+    .map((pokemon) => {
       return ` 
     <main class="card-container">
     <div class="card">
-      <img class="card__image" src="${imageOfPokemon}" alt="Image of pokemon"/>
+      <img class="card__image" src="${pokemon.sprite}" alt="Image of pokemon"/>
       <div class="card__content">
-        <div class="card__heading">${nameOfPokemon}</div>
-        <p class="card__text">${typeOfPokemon}</p>
+        <div class="card__heading">${pokemon.name}</div>
+        <p class="card__text">${pokemon.name} (#${pokemon.id}) is a ${pokemon.type[0]} & ${pokemon.type[1]} type pokemon</p>
       </div>
     `;
     })
